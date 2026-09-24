1e32d16545fd74a91a4ccba0e02797780778eda1
Matheus Polanczyk <matheus.polanczyk@hotmail.com>
2026-09-24 20:22:11 +0100

    Corrigir título da aba do site

--- script.js
+++ script.js
@@ -1,2 +1,1 @@
-// Personalize the profile and replace the example destinations in index.html.
-document.title = "Meus links | Matheus";
+// Personalize the profile and replace link destinations in index.html.
